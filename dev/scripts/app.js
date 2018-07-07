2f11d1270484f9c3fdfad8c0d4191df1173145c0
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2018-07-07 17:55:24 -0400

    added bg images, fixed up nav

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,10 +1,3 @@
-// $("a").each(function () {
-//     if ($(this).attr('href') === window.location.pathname) {
-//     	console.log('test');
-//         $(this).css('display', 'none');
-//     }
-// });
-
 $(document).ready(function() {
     $("[href]").each(function() {
         if (this.href == window.location.href) {
