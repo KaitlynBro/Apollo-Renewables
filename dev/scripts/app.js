9a4e9091f85362151cc450a7f13e3536b96831ff
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2018-09-22 13:32:21 -0400

    trying to fix calc on mobile

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -7,34 +7,6 @@ $(document).ready(function() {
     });
 });
 
-//calculation set #1 - remove if peter says calc is wrong
-
-// $( document ).ready(function() {
-//   var ELECTRICITY_GROWTH_RATE = 1.06
-  
-//   $("#range").on("input change", rangeUpdated);
-//   $("#current-monthly-cost").on("input change", costUpdated);
-
-//   function costUpdated() {
-//     var currentCost = this.value    
-//     var years = $("#range").val();
-//     calculateTotalCost(currentCost, years)    
-//   }
-  
-//   function rangeUpdated() {
-//     var years = this.value;
-//     var currentCost = $("#current-monthly-cost").val();
-//     calculateTotalCost(currentCost, years)
-//   }
-  
-//   function calculateTotalCost(currentCost, years) {
-//     if (!currentCost || !years) return;
-//     var totalCost = (currentCost * 12) * Math.pow(ELECTRICITY_GROWTH_RATE, years);
-//     $(".home-range-result").text("$" + Math.round(totalCost))
-//   }
-// });
-
-// calculation set #2 - refactor if peter says this calc is better/correct
 
 $( document ).ready(function() {
   var ELECTRICITY_GROWTH_RATE = 1.06;
